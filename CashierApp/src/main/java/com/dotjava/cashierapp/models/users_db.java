8e1a0c6d28afbf0b7dee070a8bc2933933682090
Rafael Simarmata <80381531+Rafaellsimarmata@users.noreply.github.com>
2024-03-21 07:53:53 +0700

    added cashier service

--- CashierApp/src/main/java/com/dotjava/cashierapp/models/users_db.java
+++ CashierApp/src/main/java/com/dotjava/cashierapp/models/users_db.java
@@ -1,4 +1,9 @@
 package com.dotjava.cashierapp.models;
 
 public class users_db {
+    public static Boolean Register(String username, String password, String confirmPassword, String name){
+
+
+        return false;
+    }
 }

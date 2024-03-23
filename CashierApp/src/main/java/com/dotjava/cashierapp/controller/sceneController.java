8e2b31ef8da7dc5af6a5bedc05c3323fcefd4c74
Rafael Simarmata <80381531+Rafaellsimarmata@users.noreply.github.com>
2024-03-23 15:19:32 +0700

    updated

--- CashierApp/src/main/java/com/dotjava/cashierapp/controller/sceneController.java
+++ CashierApp/src/main/java/com/dotjava/cashierapp/controller/sceneController.java
@@ -1,25 +1,35 @@
 package com.dotjava.cashierapp.controller;
 
-import com.dotjava.cashierapp.Main;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
-import java.util.Objects;
 
 public class sceneController {
 
     public static void switchToMainApp(javafx.event.ActionEvent event) throws IOException {
-
         FXMLLoader fxmlLoader = new FXMLLoader(sceneController.class.getResource("/com/dotjava/cashierapp/index.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 754, 639);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+    }
 
+    public static void switchToLogin(javafx.event.ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(sceneController.class.getResource("/com/dotjava/cashierapp/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void switchToRegister(javafx.event.ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(sceneController.class.getResource("/com/dotjava/cashierapp/register.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }

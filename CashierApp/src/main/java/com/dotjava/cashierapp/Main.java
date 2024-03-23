0a5b00c4d7bb3bb9c3387850215a69a01abfa222
Rafael Simarmata <80381531+Rafaellsimarmata@users.noreply.github.com>
2024-03-23 11:35:24 +0700

    updated auth system

--- CashierApp/src/main/java/com/dotjava/cashierapp/Main.java
+++ CashierApp/src/main/java/com/dotjava/cashierapp/Main.java
@@ -3,19 +3,27 @@ package com.dotjava.cashierapp;
 import com.dotjava.cashierapp.config.db_config;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    public static Scene scene;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("index.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 754, 639);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+        scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("CashierApp!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void setRoot(AnchorPane pane, Parent parent) {
+        pane.getChildren().clear();
+        pane.getScene().setRoot(parent);
     }
 
     public static void main(String[] args) {
